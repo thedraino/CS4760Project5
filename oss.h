35e8d94d1ea6892286b686fff75e5ce6bc55c41f
andrew audrain <audrain@hoare7.cs.umsl.edu>
2018-11-12 18:25:18 -0600

    Added stdio.h library to test Makefile.

---
+++ oss.h
@@ -0,0 +1,11 @@
+// File: oss.h
+// Created by: Andrew Audrain
+//
+// Header file to be used with oss.c and user.c
+
+#ifndef OSS_HEADER_FILE
+#define OSS_HEADER_FILE
+
+#include <stdio.h>
+
+#endif 
