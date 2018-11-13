9adf7c66a8af4bc2732901bcca96abc7854530af
andrew audrain <audrain@hoare7.cs.umsl.edu>
2018-11-12 18:25:55 -0600

    Added header file to test Makefile.

---
+++ oss.c
@@ -0,0 +1,12 @@
+// File: oss.c | Executable (after make): oss
+// Created by: Andrew Audrain
+//
+// Master process to simulate a resource management module
+
+#include "oss.h"
+
+int main ( int argc, char *argv[] ) {
+	printf ( "Hello, from OSS.\n" );
+
+	return 0;
+}
