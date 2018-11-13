238ce4e21725846ced8a90f2b6627d96a3353291
andrew audrain <audrain@hoare7.cs.umsl.edu>
2018-11-12 18:26:12 -0600

    Added header file to test Makefile.

---
+++ user.c
@@ -0,0 +1,13 @@
+// File: user.c | Executable: user
+// Created by: Andrew Audrain
+//
+// USER process that requests and releases resources.
+// Generated and managed by OSS. 
+
+#include "oss.h"
+
+int main ( int argc, char *argv[] ) {
+	printf ( "Hello, from a USER process.\n" );
+
+	return 0;
+}
