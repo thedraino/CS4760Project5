4454a015b15e0cd6e824fcec4d3f6b29dd5d2624
andrew audrain <audrain@hoare7.cs.umsl.edu>
2018-11-13 21:57:31 -0600

    Added in some structures, variables, and func prototypes that are used in both OSS and USER.

--- oss.h
+++ oss.h
@@ -7,5 +7,48 @@
 #define OSS_HEADER_FILE
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <signal.h>
+#include <errno.h>
+#include <limits.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <stdbool.h>
+
+/* Structure(s) */
+// Structure used in the message queue 
+typedef struct {
+	long msg_type;		// Controls who can receive the message.
+	int pid;		// Store the pid of the sender.
+	int tableIndex;		// Store the index of the child process from USER.
+	int request;		// Some value from 0-19 if the child process is requesting a resource from OSS.
+	int release;		// Some value from 0-19 if the child is notifying OSS that it is releasing a resource.
+	bool terminate;		// Default is false. Gets changed to true when child terminates. 
+	bool resourceGranted;	// Default is false. Gets changed to true when OSS approves the resource request from USER. 
+} Message;
+
+/* Function Prototypes */
+void handle ( int sig_num );	// Function to handle the alarm or ctrl-c signals
+
+/* Message Queue Variables */
+Message message;
+int messageID;
+key_t messageKey;
+
+/* Shared Memory Variables */
+int shmClockID;
+int *shmClock;
+key_t shmClockKey;
+
+int shmBlockedID;
+int *shmBlocked;
+key_t shmBlockedKey;
 
 #endif 
